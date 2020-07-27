b5d0dad01308df2847fd47e0d5c208158d99aed3
유창선 <ycs5682@gmail.com>
2020-07-27 21:32:00 +0900

    x ボタンをクリックするか検索を削除すると検索結果が消える(console.log)

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,12 +4,16 @@ const tag = '[MainController]';
 
 export default {
   init() {
-    FormView.setup(document.querySelector('form')).on('@submit', (e) =>
-      this.onSubmit(e.detail.input)
-    );
+    FormView.setup(document.querySelector('form'))
+      .on('@submit', (e) => this.onSubmit(e.detail.input))
+      .on('@reset', () => this.onResetForm());
   },
 
   onSubmit(input) {
     console.log(tag, 'onSubmit()', input);
+  },
+
+  onResetForm() {
+    console.log(tag, 'onResetForm()');
   }
 };
